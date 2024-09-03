e22aa02621283f03dbceb33ba8223cd367c07a89
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-09-03 19:45:45 +0200

    three routes etablished but it is temporary

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './components/HomePage';
+import GamePage from './components/GamePage';
+import NotFoundPage from './components/NotFoundPage';
 
 function App() {
 
 	return (
-		<>
-			<div>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<HomePage/>} />
-					</Routes>
-				</BrowserRouter>
-			</div>
-		</>
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/room/player" element={<GamePage />} />
+					<Route path="/*" element={<NotFoundPage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
 	)
 }
 
