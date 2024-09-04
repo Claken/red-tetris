074cdc82290c32a5746c13a1bca525d09c791d7b
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-09-04 20:35:39 +0200

    add functions to the home page

--- my-app/src/components/HomePage.tsx
+++ my-app/src/components/HomePage.tsx
@@ -2,22 +2,32 @@ import React, { useState } from 'react';
 
 function HomePage() {
 
-	const [text, setText] = useState('');
+	const [name, setName] = useState('');
 
 	const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setText(event.target.value);
+		event.preventDefault();
+		setName(event.target.value);
 	  };
+	
+	const handleSubmit = () => {
+		if (name.length == 0) {
+			console.log("the name is empty");
+		}
+		else {
+			console.log(name + " is good");
+		}
+	}
 
 	return (
 		<div>
 			<h1>Please write your player name to access the site</h1>
 			<textarea
-        		value={text}
+        		value={name}
         		onChange={handleChange}
        			placeholder="Type here..."
       		/>
 			<div></div>
-			 <button onClick={() => {}}>Confirm</button>
+			 <button onClick={handleSubmit}>Confirm</button>
 		</div>
 	);
 }
