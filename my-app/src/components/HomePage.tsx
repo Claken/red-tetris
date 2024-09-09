e30233ebc785ec87e2cc349883689977c6f6093d
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-09-09 16:25:42 +0200

    design for homepage in progress

--- my-app/src/components/HomePage.tsx
+++ my-app/src/components/HomePage.tsx
@@ -8,8 +8,8 @@ function HomePage() {
 	const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
 		event.preventDefault();
 		setName(event.target.value);
-	  };
-	
+	};
+
 	const handleSubmit = () => {
 		if (name.length == 0) {
 			console.log("the name is empty");
@@ -21,16 +21,16 @@ function HomePage() {
 
 	return (
 		<div>
-			<h1 className="text-red-500">Please write your player name to access the site</h1>
-			<textarea
-        		value={name}
-        		onChange={handleChange}
-       			placeholder="Type here..."
-      		/>
-			<div className="text-red-500 font-bold">
-				TEFJEZOFKEO
+			<h1 className="text-red-500 font-bold">Please write your player name to access the site</h1>
+			<div>
+				<input 	type="text"
+						className="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+						placeholder="Type here..."
+						onChange={() => handleChange}
+						/>
 			</div>
-			 <button onClick={handleSubmit}>Confirm</button>
+			<div></div>
+			<button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleSubmit}>Confirm</button>
 		</div>
 	);
 }
