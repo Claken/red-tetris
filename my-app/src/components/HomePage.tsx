c3bc05fe4730e5e7fa3037fa402283481950e332
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-09-10 20:53:16 +0200

    handleSubmit functions

--- my-app/src/components/HomePage.tsx
+++ my-app/src/components/HomePage.tsx
@@ -5,7 +5,7 @@ function HomePage() {
 
 	const [name, setName] = useState('');
 
-	const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		event.preventDefault();
 		setName(event.target.value);
 	};
@@ -22,15 +22,16 @@ function HomePage() {
 	return (
 		<div>
 			<h1 className="text-red-500 font-bold">Please write your player name to access the site</h1>
+			<div className="m-5"></div>
 			<div>
-				<input 	type="text"
-						className="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-						placeholder="Type here..."
-						onChange={() => handleChange}
-						/>
+				<input type="text"
+					className="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+					placeholder="Type here..."
+					onChange={handleChange}
+				/>
 			</div>
-			<div></div>
-			<button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleSubmit}>Confirm</button>
+			<div className="m-5"></div>
+			<button className="bg-red-500 hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 rounded-full" onClick={handleSubmit}>Confirm</button>
 		</div>
 	);
 }
