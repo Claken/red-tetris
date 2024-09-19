9c5a2e258de64acb3c4cc38669484bf8e643a367
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-09-19 21:59:30 +0200

    home page in progress + id and setId in context

--- my-app/src/components/HomePage.tsx
+++ my-app/src/components/HomePage.tsx
@@ -12,11 +12,20 @@ function HomePage() {
 
 	return (
 		playerContext.name ?
-		<div className="bg-black h-screen">
-			
-		</div>
-		: <ConnectPage/>
+			<div className="bg-black h-screen">
+				<div className="flex items-center justify-center h-screen">
+					<div className="relative border-4 border-red-500 w-64 h-96">
+						<div className='text-center space-y-3'>
+							<button className="bg-red-500 hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 rounded-full" onClick={() => { }}>Join a solo game</button>
+							<button className="bg-red-500 hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 rounded-full" onClick={() => { }}>Join a game</button>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			: <ConnectPage />
 	);
 }
 
 export default HomePage;
+
