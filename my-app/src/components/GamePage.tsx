c6b238a57304ef04b1bb8870b7335d1f3dc09f1a
Claken <sachouam@student.42.fr>
2024-09-21 23:54:18 +0200

    grid in progress

--- my-app/src/components/GamePage.tsx
+++ my-app/src/components/GamePage.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 function GamePage() {
-  return (
-	<div className="bg-black h-screen">
-		<div className="flex items-center justify-center h-screen">
-		<h1 className="text-white text-3xl">RED TETRIS</h1>
+
+	const numRows = 20;
+	const numCols = 10;
+
+	const [grid, setGrid] = useState(Array.from({ length: numRows }, () =>
+		Array(numCols).fill(0)
+	));
+
+
+
+	return (
+		<div className="bg-black h-screen">
+			<div className="flex items-center justify-center h-screen">
+				<div className="grid grid-cols-10 gap-0.5">
+					{grid.map((row, rowIndex) =>
+						row.map((cell, colIndex) => (
+							<div
+								key={`${rowIndex}-${colIndex}`}
+								className={`w-8 h-8 border border-red-700 ${cell ? 'bg-red-500' : 'bg-red-900'}`}
+							></div>
+						))
+					)}
+				</div>
+			</div>
 		</div>
-	</div>
-  );
+	);
 }
 
 export default GamePage;
