327073b3b652f729e15e6c3341e6fa9af5e19724
Claken <sachouam@student.42.fr>
2024-09-22 00:31:19 +0200

    borded added

--- my-app/src/components/GamePage.tsx
+++ my-app/src/components/GamePage.tsx
@@ -14,15 +14,19 @@ function GamePage() {
 	return (
 		<div className="bg-black h-screen">
 			<div className="flex items-center justify-center h-screen">
-				<div className="grid grid-cols-10 gap-0.5">
-					{grid.map((row, rowIndex) =>
-						row.map((cell, colIndex) => (
-							<div
-								key={`${rowIndex}-${colIndex}`}
-								className={`w-8 h-8 border border-red-700 ${cell ? 'bg-red-500' : 'bg-red-900'}`}
-							></div>
-						))
-					)}
+				<div className="border-8 border-red-500">
+				<div className="border-2 border-black">
+					<div className="grid grid-cols-10 gap-0.5">
+						{grid.map((row, rowIndex) =>
+							row.map((cell, colIndex) => (
+								<div
+									key={`${rowIndex}-${colIndex}`}
+									className={`w-8 h-8 border border-red-700 ${cell ? 'bg-red-500' : 'bg-red-900'}`}
+								></div>
+							))
+						)}
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
