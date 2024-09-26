9109d0d66f3da3ea90f74f9fa2f0fb7b8f0bff6b
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-09-26 18:57:12 +0200

    new test added

--- my-app/src/tests/GamePage.test.tsx
+++ my-app/src/tests/GamePage.test.tsx
@@ -18,3 +18,16 @@ test('placeTetromino', () => {
     const newGrid = placeTetromino(grid, tetromino, 1, 1);
     expect(newGrid).toEqual(expectedGrid);
 })
+
+test('it should not modify the original grid', () => {
+  const grid = Array.from({ length: 5 }, () => Array(5).fill(0));
+  const tetromino = [
+    [1, 1],
+    [1, 1],
+  ];
+
+  const originalGrid = [...grid.map(row => [...row])];
+  placeTetromino(grid, tetromino, 2, 2);
+
+  expect(grid).toEqual(originalGrid);
+});
