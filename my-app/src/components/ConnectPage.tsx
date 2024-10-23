5cb127cf1ec2dfce09328636fcd76ac55c751775
sachou <sachou@UBUNTU.myguest.virtualbox.org>
2024-10-23 16:25:56 +0200

    new design for connect page, but might change late

--- my-app/src/components/ConnectPage.tsx
+++ my-app/src/components/ConnectPage.tsx
@@ -26,13 +26,13 @@ function ConnectPage({ name, setName, uuid, setUuid, socket, setSocket }: {
 	}
 
 	// useEffect(() => {
-		socket?.on("new-person", (data) => {
-			console.log("new-person");
-			console.log(data);
-			sessionStorage.setItem("uuid", data.uuid);
-			sessionStorage.setItem("name", data.name);
-			setUuid(data.uuid);
-		});
+	socket?.on("new-person", (data) => {
+		console.log("new-person");
+		console.log(data);
+		sessionStorage.setItem("uuid", data.uuid);
+		sessionStorage.setItem("name", data.name);
+		setUuid(data.uuid);
+	});
 	// 	return () => {
 	// 		socket?.off("new-person");
 	// 	}
@@ -40,19 +40,17 @@ function ConnectPage({ name, setName, uuid, setUuid, socket, setSocket }: {
 
 	return (
 		<div className="flex items-center justify-center h-screen bg-black">
-			<div className="text-center">
-				<h1 className="text-red-500 font-bold">WELCOME TO RED TETRIS</h1>
-				<h1 className="text-red-500 font-bold">Please write your player name to access the site</h1>
-				<div className="m-5"></div>
-				<div>
-					<input type="text"
+			<div className="flex flex-col justify-center items-center">
+				<div className="text-white text-3xl font-bold p-2 m-4 bg-gray-900 border-4 border-red-500 rounded-full w-fit">WELCOME TO RED TETRIS</div>
+				<div className="text-white font-bold p-2 bg-gray-900 border-2 border-red-500 rounded-full w-fit">Please write your player name to access the site</div>
+				<div className="m-3">
+					<input type="text-center"
 						className="bg-transparent placeholder:text-white text-white text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
 						placeholder="Type here..."
 						onChange={handleChange}
 					/>
 				</div>
-				<div className="m-5"></div>
-				<button className="bg-red-500 hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 rounded-full" onClick={handleSubmit}>Confirm</button>
+					<button className="bg-red-500 hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 rounded-full w-fit" onClick={handleSubmit}>Confirm</button>
 			</div>
 		</div>
 	);
