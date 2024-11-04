fdcfce5fe9fa77a32d28cecef5f78f33f8b204e3
Claken <sachouam@student.42.fr>
2024-11-04 19:27:00 +0100

    new design for connect page

--- my-app/src/components/ConnectPage.tsx
+++ my-app/src/components/ConnectPage.tsx
@@ -35,17 +35,17 @@ function ConnectPage({ name, setName, uuid, setUuid, socket, setSocket }: {
 
 	return (
 		<div className="flex items-center justify-center h-screen bg-black">
-			<div className="flex flex-col justify-center items-center">
-				<div className="text-white text-3xl font-bold p-2 m-4 bg-gray-900 border-4 border-red-500 rounded-full w-fit">WELCOME TO RED TETRIS</div>
-				<div className="text-white font-bold text-sm p-2 bg-gray-900 w-fit">Please write your player name to access the site</div>
+			<div className="flex flex-col justify-center items-center rounded-md bg-red-500">
+				<div className="text-white text-3xl font-bold p-2 m-4 bg-red-500 border-4 border-red-500 w-fit">WELCOME TO RED TETRIS</div>
+				<div className="text-white text-center font-bold text-sm p-2 bg-red-900 border border-white w-full">↓ Please write your player name to access the site ↓</div>
 				<div className="m-3">
-					<input type="text-center"
-						className="bg-transparent placeholder:text-white text-white text-xs border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-						placeholder="Type here..."
+					<input type="text"
+						className="bg-red-900 placeholder:text-white placeholder:opacity-60 text-white font-bold border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+						placeholder="Player name"
 						onChange={handleChange}
 					/>
 				</div>
-					<button className="bg-red-500 hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 rounded-full w-fit" onClick={handleSubmit}>Confirm</button>
+					<button className="bg-red-900 rounded-b-md hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 w-full" onClick={handleSubmit}>Confirm</button>
 			</div>
 		</div>
 	);
