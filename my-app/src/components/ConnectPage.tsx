f3399edc0585be0efbc46ad26d4cc46299264b3c
Claken <sachouam@student.42.fr>
2024-11-06 00:48:22 +0100

    new design for connect page 3

--- my-app/src/components/ConnectPage.tsx
+++ my-app/src/components/ConnectPage.tsx
@@ -35,9 +35,9 @@ function ConnectPage({ name, setName, uuid, setUuid, socket, setSocket }: {
 
 	return (
 		<div className="flex items-center justify-center h-screen bg-black">
-			<div className="flex flex-col justify-center items-center rounded-md bg-red-500">
-				<div className="text-white text-3xl font-bold p-2 m-4 bg-red-500 border-4 border-red-500 w-fit">WELCOME TO RED TETRIS</div>
-				<div className="text-white text-center font-bold text-sm p-2 bg-red-900 border border-white w-full">↓ Please write your player name to access the site ↓</div>
+			<div className="flex flex-col justify-center items-center border border-white rounded-md bg-red-500">
+				<div className="text-white text-3xl font-bold p-2 m-4 w-fit">WELCOME TO RED TETRIS</div>
+				<div className="text-white text-center font-bold text-sm p-2 bg-red-900 border-y border-white w-full">↓ Please write your player name to access the site ↓</div>
 				<div className="m-3">
 					<input type="text"
 						className="bg-red-900 placeholder:text-white placeholder:opacity-60 text-white font-bold border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
@@ -45,7 +45,7 @@ function ConnectPage({ name, setName, uuid, setUuid, socket, setSocket }: {
 						onChange={handleChange}
 					/>
 				</div>
-					<button className="bg-red-900 rounded-b-md hover:bg-red-700 active:bg-red-500 text-white font-bold py-2 px-4 w-full" onClick={handleSubmit}>CONFIRM</button>
+					<button className="bg-red-900 rounded-b-md hover:bg-red-700 border-t border-white active:bg-red-500 text-white font-bold py-2 px-4 w-full" onClick={handleSubmit}>CONFIRM</button>
 			</div>
 		</div>
 	);
