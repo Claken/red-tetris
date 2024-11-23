e7c5837e83972b692ef5428043c48be088c07de9
raphael khelif <raphi031@hotmail.fr>
2024-11-23 20:03:04 +0100

    add some test

--- server/class/tetromino/tetromino.spec.ts
+++ server/class/tetromino/tetromino.spec.ts
@@ -1,4 +1,5 @@
 import { Tetromino } from './tetromino';
+import { I, E } from '../../constantes/constantes';
 
 const tetrominoData = [
   {
@@ -313,5 +314,89 @@ describe('Tetromino', () => {
         expect(t.getRotation()).toEqual(valTest.rotation);
       }
     }
+  });
+
+  describe('getLentgth()', () => {
+    it('should return correct dimensions and first non-empty cell positions', () => {
+      // Arrange
+      const shape = [
+        [E, E, E, E],
+        [E, I, I, E],
+        [E, E, E, E],
+      ];
+      const tetromino = new Tetromino(0, shape, 'I');
+
+      // Act
+      const result = tetromino.getLentgth();
+
+      // Assert
+      expect(result).toEqual({
+        x: 4, // Width of the shape
+        y: 3, // Height of the shape
+        lengthYBeforeNumber: 1, // Row index of the first non-empty cell
+        lengthXBeforeNumber: 1, // Column index of the first non-empty cell
+      });
+    });
+
+    it('should return zeros for empty shape', () => {
+      // Arrange
+      const tetromino = new Tetromino();
+
+      // Act
+      const result = tetromino.getLentgth();
+
+      // Assert
+      expect(result).toEqual({
+        x: 0,
+        y: 0,
+        lengthYBeforeNumber: 0,
+        lengthXBeforeNumber: 0,
+      });
+    });
+  });
+  describe('getShape()', () => {
+    it('should return the current shape of the Tetromino', () => {
+      // Arrange
+      const shape = [
+        [E, E, E],
+        [E, I, E],
+        [E, E, E],
+      ];
+      const tetromino = new Tetromino(0, shape, 'I');
+
+      // Act
+      const result = tetromino.getShape();
+
+      // Assert
+      expect(result).toEqual(shape);
+    });
+  });
+
+  describe('getRotation()', () => {
+    it('should return the current rotation of the Tetromino', () => {
+      // Arrange
+      const rotation = 90;
+      const tetromino = new Tetromino(rotation, [], 'I');
+
+      // Act
+      const result = tetromino.getRotation();
+
+      // Assert
+      expect(result).toBe(rotation);
+    });
+  });
+
+  describe('getType()', () => {
+    it('should return the type of the Tetromino', () => {
+      // Arrange
+      const type = 'I';
+      const tetromino = new Tetromino(0, [], type);
+
+      // Act
+      const result = tetromino.getType();
+
+      // Assert
+      expect(result).toBe(type);
+    });
   });
 });
