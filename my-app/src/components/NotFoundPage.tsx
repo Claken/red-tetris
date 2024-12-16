a38bc411c1553e8953b1f54ac3f349b5a761aaf6
Claken <sachouam@student.42.fr>
2024-12-16 19:23:46 +0100

    new tests again

--- my-app/src/components/NotFoundPage.tsx
+++ my-app/src/components/NotFoundPage.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
 function NotFoundPage() {
   return (
-	<div className="bg-black h-screen">
+	<div data-testid="not-found-page" className="bg-black h-screen">
 		<div className="flex items-center justify-center h-screen">
 		<h1 className="text-white text-3xl">404 - NOT FOUND</h1>
 		</div>
